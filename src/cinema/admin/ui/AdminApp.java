087d92653be99505434879c55263c682700fe5dc
Yasa Akbulut <yasakbulut@gmail.com>
2014-07-10 16:55:55 +0300

    add DB capabilities to AdminApp

--- src/cinema/admin/ui/AdminApp.java
+++ src/cinema/admin/ui/AdminApp.java
@@ -1,17 +1,33 @@
 package cinema.admin.ui;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 import cinema.model.Hall;
+import cinema.repository.HallRepository;
 
 public class AdminApp {
+	private static final String DB_URL = "jdbc:postgresql://localhost/cinema";
+	private static final String DB_USER = "postgres";
+	private static final String DB_PASS = "1234";
 	
 	public static void main(String[] args) {
-		List<Hall> halls = new ArrayList<>();
 		Scanner scanner = new Scanner(System.in);
 		String current = "";
+		Connection connection;
+		HallRepository repository = null;
+		try {
+			connection = getConnection();
+			repository = new HallRepository(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		List<Hall> halls = new ArrayList<>(repository.getAll());
 		while(true){
 			System.out.println("Enter a hall (rowsxcols):");
 			current = scanner.nextLine();
@@ -25,13 +41,25 @@ public class AdminApp {
 			String name = scanner.nextLine();
 			
 			Hall hall = new Hall(rows, cols, name);
-			halls.add(hall);
+			Hall savedHall = repository.saveHall(hall);
+			halls.add(savedHall);
 			System.out.println("Hall created.");			
 		}
 		scanner.close();
 		
 		System.out.println(halls);
 		
+	}
+	
+	private static Connection getConnection() throws SQLException{
+		try {
+			Class.forName("org.postgresql.Driver"); 
+			System.out.println("Driver loaded.");
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+			System.exit(-1);
+		}
+		return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
 	}
 
 }
