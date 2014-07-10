f618a0e0fed1926da2ce7dec9859207a85e3baf4
Yasa Akbulut <yasakbulut@gmail.com>
2014-07-10 15:01:56 +0300

    add equals, hashCode and getEmptySeats to Hall model;

--- src/cinema/model/Hall.java
+++ src/cinema/model/Hall.java
@@ -1,5 +1,8 @@
 package cinema.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Hall {
 
 	private int id;
@@ -38,6 +41,39 @@ public class Hall {
 	public String toString() {
 		return "Hall [id=" + id + ", rows=" + rows + ", cols=" + cols
 				+ ", name=" + name + "]";
+	}
+
+	public boolean[][] getEmptySeats(List<Ticket> tickets, Hour hour) {
+
+		boolean[][] emptySeats = new boolean[rows][cols];
+		for (int i = 0; i < emptySeats.length; i++) {
+			boolean[] currentRow = new boolean[cols];
+			Arrays.fill(currentRow, true);
+			emptySeats[i] = currentRow;
+		}
+
+		for (Ticket ticket : tickets) {
+			if (ticket.getHour().equals(hour) && ticket.getHall().equals(this)) {
+				emptySeats[ticket.getRow()][ticket.getCol()] = false;
+			}
+		}
+
+		return emptySeats;
+
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Hall)) {
+			return false;
+		}
+		Hall other = (Hall) obj;
+		return this.getName().equals(other.getName());
+	}
+
+	@Override
+	public int hashCode() {
+		return getName().hashCode();
 	}
 
 }
